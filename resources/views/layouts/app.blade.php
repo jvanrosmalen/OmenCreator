0072b99243c49544bf5809093ea6dee9648a297a
jvanrosmalen <jazz_cama@hotmail.com>
2018-07-01 16:17:01 +0200

    changes for deployment

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <base href="http://student.aii.avans.nl/DOC/jrosmale/OmenCreator/public/">
+    <!-- <base href="http://student.aii.avans.nl/DOC/jrosmale/OmenCreator/public/"> -->
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -9,10 +9,13 @@
     <title>Omen Creator</title>
     
     <!-- Need this below for all the links in my ajaxInterface.js -->
-	<script>
+	<!-- <script>
 		var GLOBAL_BASE = 'http://student.aii.avans.nl/DOC/jrosmale/OmenCreator/public/';
+	</script> -->
+    <script>
+		var GLOBAL_BASE = '';
 	</script>
-	
+
     <!-- Fonts -->
 	<link href="{{ URL::asset('css/font-awesome-4.7.0/css/font-awesome.min.css') }}" rel='stylesheet' type='text/css'>
     <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700" rel='stylesheet' type='text/css'>
