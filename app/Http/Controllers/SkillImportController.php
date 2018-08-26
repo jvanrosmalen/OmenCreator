b3e3fbd19f2a8eec24b96284ebc02853699e0909
jvanrosmalen <jazz_cama@hotmail.com>
2018-08-26 12:08:02 +0200

    import skills

--- app/Http/Controllers/SkillImportController.php
+++ app/Http/Controllers/SkillImportController.php
@@ -6,6 +6,8 @@ use App\Http\Requests;
 use Illuminate\Support\Facades\Input;
 use Request;
 use Session;
+use PHPExcel; 
+use PHPExcel_IOFactory;
 
 class SkillImportController extends Controller
 {
@@ -20,9 +22,33 @@ class SkillImportController extends Controller
             if(strcasecmp($importFile->getClientOriginalExtension(),"xlsx") != 0){
                 return view('/skill/shownoimportfilewarning');    
             }
-			$skill->skill_handout = $importFile->getClientOriginalName();
+            
+            // There is a file, and it is a xlsx file. Let's try and make something useful out of it.
+
+            // Move file to known place on server
+            $importFilePath = 
+            $this->handleImportFile($importFile);
 		} else {
 			return view('/skill/shownoimportfilewarning');
 		}		
-	}
+    }
+    
+    private function moveFileToServer($file){
+        // Moves file to storage and returns path.
+        Storage::put(
+            'skillimports/'.$file->getClientOriginalName(),
+            file_get_contents($file->getRealPath())
+        );
+
+        return Storage::url('skillimports/'.$file->getClientOriginalName());
+    }
+
+    private function handleImportFile($importFile){
+        $objPHPExcel = PHPExcel_IOFactory::load($path);
+        $objWorksheet = $objPHPExcel->getActiveSheet();
+        $highestRow = $objWorksheet->getHighestRow();
+        for ($row = 1; $row <= $highestRow; ++$row) {
+             var_dump($objWorksheet->getCellByColumnAndRow(1, $row));
+        }
+    }
 }
