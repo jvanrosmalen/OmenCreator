3ec3ef5668ef746eb452db39e6423338b4ae2dbb
jvanrosmalen <jazz_cama@hotmail.com>
2018-09-11 17:21:23 +0200

    skill import

--- resources/views/skill/showImportLog.blade.php
+++ resources/views/skill/showImportLog.blade.php
@@ -5,32 +5,33 @@
 @section('content')
     <div class='container'>
         <div class='row'>
-            <div class="col-xs-1">
-            </div>
-            <div class='col-xs-10'>
+            <div class='col-xs-12'>
                 <h3>Import Resultaten</h3>
             </div>
         </div>
-        @foreach($errorLogArray as $skillName => $errors)
-            <div class="row">
-                <div class="col-xs-1">
+
+        <div class="errorLogOverview">
+            @foreach($errorLogArray as $skillName => $errors)
+                <div class="row">
+                    <div class="col-xs-1">
+                    </div>
+                    <div class='col-xs-10'>
+                        <h5>{{$skillName}}</h5>
+                    </div>
                 </div>
-                <div class='col-xs-10'>
-                    <h4>{{$skillName}}</h4>
+                <div class="row">
+                    <div class="col-xs-2">
+                    </div>
+                    <div class='col-xs-10'>
+                        <ul class="errorLog">
+                        @foreach($errors as $error)
+                            <li>{{$error}}</li>
+                        @endforeach
+                        </ul>
+                    </div>
                 </div>
-            </div>
-            <div class="row">
-                <div class="col-xs-2">
-                </div>
-                <div class='col-xs-10'>
-                    <ul class="errorLog">
-                    @foreach($errors as $error)
-                        <li>{{$error}}</li>
-                    @endforeach
-                    </ul>
-                </div>
-            </div>
-        @endforeach
+            @endforeach
+        </div>
     </div>
 @endsection
 
