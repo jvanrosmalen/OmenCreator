f028de1c0b47beb5a3ccfe1cc88e77ad85e7f717
jvanrosmalen <jazz_cama@hotmail.com>
2018-09-11 16:39:34 +0200

    skill import

--- resources/views/skill/importlog.blade.php
+++ resources/views/skill/importlog.blade.php
@@ -2,27 +2,10 @@
 <html lang="en">
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-        <style>
-            li a:before {
-                content: "\2610";
-                margin-right:5px;
-            }
-        </style>
     </head>
 
     <body>
-        <div class="row">
-            <div class="col-xs-2">
-            </div>
-            <div class="col-xs-4">
-                <h3>Import Log</h3>
-            </div>
-            <div class="col-xs-4">
-                <h4>
-
-                </h4>
-            </div>
-        </div>
+            <h1>Test test</h1>
 
     </body>   
 </html>
