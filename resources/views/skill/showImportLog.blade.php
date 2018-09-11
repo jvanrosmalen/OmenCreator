dc1ce658d5205ed9dac8986bd2abaa2f3fcb9a67
jvanrosmalen <jazz_cama@hotmail.com>
2018-09-11 16:58:31 +0200

    skill import

--- resources/views/skill/showImportLog.blade.php
+++ resources/views/skill/showImportLog.blade.php
@@ -5,11 +5,33 @@
 @section('content')
     <div class='container'>
         <div class='row'>
-            <div class='col-xs-12'>
+            <div class="col-xs-1">
+            </div>
+            <div class='col-xs-10'>
                 <h3>Import Resultaten</h3>
             </div>
         </div>
     </div>
+        @foreach($errorLogArray as $skillName => $errors)
+            <div class="row">
+                <div class="col-xs-1">
+                </div>
+                <div class='col-xs-10'>
+                    <h4>{{$skillName}}</h4>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-2">
+                </div>
+                <div class='col-xs-10'>
+                    <ul class="errorLog">
+                    @foreach($errors as $error)
+                        <li>{{$error}}</li>
+                    @endforeach
+                    </ul>
+                </div>
+            </div>
+        @endforeach
 @endsection
 
 </html>
