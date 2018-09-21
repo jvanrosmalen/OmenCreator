45b2cb02ae3c26cc25f2320c3ec1972c7f945df8
jvanrosmalen <jazz_cama@hotmail.com>
2018-09-21 15:53:43 +0200

    password reset

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -11,6 +11,7 @@
                     <div class="text-center">
                         Kijk eens aan. Al weer bijna helemaal genezen. Nog een laatste paar kleine vraagjes, heb ik voor je.<br>
                         Als je die ingevuld hebt, verklaar ik je genezen, en kan je weer gaan en staan waar je wil.
+                        <br><br>
                     </div>
 
                     <form class="form-horizontal" role="form" method="POST" action="{{ url('/password/reset') }}">
@@ -19,7 +20,7 @@
                         <input type="hidden" name="token" value="{{ $token }}">
 
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label class="col-md-4 control-label">E-Mail Address</label>
+                            <label class="col-md-4 control-label">E-Mail Adres</label>
 
                             <div class="col-md-6">
                                 <input type="email" class="form-control" name="email" value="{{ $email or old('email') }}">
@@ -33,7 +34,7 @@
                         </div>
 
                         <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label class="col-md-4 control-label">Password</label>
+                            <label class="col-md-4 control-label">Wachtwoord</label>
 
                             <div class="col-md-6">
                                 <input type="password" class="form-control" name="password">
@@ -47,7 +48,7 @@
                         </div>
 
                         <div class="form-group{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
-                            <label class="col-md-4 control-label">Confirm Password</label>
+                            <label class="col-md-4 control-label">Wachtwoord Bevestigen</label>
                             <div class="col-md-6">
                                 <input type="password" class="form-control" name="password_confirmation">
 
@@ -62,7 +63,7 @@
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
                                 <button type="submit" class="btn btn-primary">
-                                    <i class="fa fa-btn fa-refresh"></i>Reset Password
+                                    <i class="fa fa-btn fa-refresh"></i>Wachtwoord Opnieuw Instellen
                                 </button>
                             </div>
                         </div>
