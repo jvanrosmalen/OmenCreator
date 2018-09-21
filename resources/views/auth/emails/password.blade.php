94f9d2ffdb589c1d84ebde3b585853f9a33a962b
jvanrosmalen <jazz_cama@hotmail.com>
2018-09-21 15:48:30 +0200

    password reset

--- resources/views/auth/emails/password.blade.php
+++ resources/views/auth/emails/password.blade.php
@@ -1,6 +1,8 @@
 <em>Vithas zegt:<br>
 Ah, je bent wakker. Mooi zo. Nog eventjes en je voelt je weer kiplekker, dat beloof ik je.<br>
 Luister naar je heelmeester en drink dit helemaal op. Ja, ik weet dat het bitter smaakt,<br>
-maar zachte heelmeesters en zo. Je kent het wel.<em><br>
+maar zachte heelmeesters en zo. Je kent het wel.</em><br>
 <br>
 Klik hier om je wachtwoord opnieuw in te stellen: <a href="{{ $link = url('password/reset', $token).'?email='.urlencode($user->getEmailForPasswordReset()) }}"> {{ $link }} </a>
+
+Dit is een automatisch gegenereerde mail. Antwoorden op deze mail heeft geen zin.
