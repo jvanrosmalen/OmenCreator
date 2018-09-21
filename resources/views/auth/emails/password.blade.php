13328a7163119f41f4df1c8c53d7f8f041994e6a
jvanrosmalen <jazz_cama@hotmail.com>
2018-09-21 15:46:28 +0200

    password reset

--- resources/views/auth/emails/password.blade.php
+++ resources/views/auth/emails/password.blade.php
@@ -1,1 +1,6 @@
-Click here to reset your password: <a href="{{ $link = url('password/reset', $token).'?email='.urlencode($user->getEmailForPasswordReset()) }}"> {{ $link }} </a>
+<em>Vithas zegt:<br>
+Ah, je bent wakker. Mooi zo. Nog eventjes en je voelt je weer kiplekker, dat beloof ik je.<br>
+Luister naar je heelmeester en drink dit helemaal op. Ja, ik weet dat het bitter smaakt,<br>
+maar zachte heelmeesters en zo. Je kent het wel.<em><br>
+<br>
+Klik hier om je wachtwoord opnieuw in te stellen: <a href="{{ $link = url('password/reset', $token).'?email='.urlencode($user->getEmailForPasswordReset()) }}"> {{ $link }} </a>
