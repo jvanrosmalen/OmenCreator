2e5dcb4bbab08ee294634a3ba7bcc9323c25e527
jvanrosmalen <jazz_cama@hotmail.com>
2018-10-09 19:30:35 +0200

    test

--- resources/views/race/showAllRaces.blade.php
+++ resources/views/race/showAllRaces.blade.php
@@ -150,9 +150,8 @@
 			</div>
 		@endforeach
 		
-		<?php var_dump($race_ids) ?>
-
 <!-- 	Trick to be able to access the count of race in the JS below -->
+		<div id="race_ids" class="hidden" data-racesids="{{ $race_ids }}" ></div>
 		
 		<script>Race.addListeners()</script>
  	</div>
