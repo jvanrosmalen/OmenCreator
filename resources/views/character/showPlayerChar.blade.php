846f972b335949f8c3a1dc48ee838ea88f829772
jvanrosmalen <jazz_cama@hotmail.com>
2018-10-16 09:44:44 +0200

    bugfix

--- resources/views/character/showPlayerChar.blade.php
+++ resources/views/character/showPlayerChar.blade.php
@@ -396,23 +396,7 @@
 									var_dump($char_docs);
 								?>
 
-								<!-- @foreach($char_docs as $char_doc)
-								<tr>
-									<td class="col-xs-9">
-										{{ $char_doc["char_doc_name"] }}
-									</td>
-									<td class="col-xs-3">
-										<a class='btn btn-success btn-xs' href="download_chardoc/{{$character->id}}/{{ $char_doc['char_name'] }}" data-toggle='tooltip' title='download document'>
-											<span class='glyphicon glyphicon-download-alt'></span>
-										</a>
-										@if( Auth::user()->is_story_telling || Auth::user()->is_admin)
-										<a class='btn btn-warning btn-xs' href="remove_chardoc/{{$character->id}}/{{ $char_doc['char_name'] }}" data-toggle='tooltip' title='verwijder document'>
-											<span class='glyphicon glyphicon-minus'></span>
-										</a>
-										@endif
-									</td>
-								</tr>
-								@endforeach -->
+
 							</tbody>
 						</table>
 					</div>
