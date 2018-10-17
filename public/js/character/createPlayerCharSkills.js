9e9bd56ba528b738d4574813b84116a58576282e
jvanrosmalen <jazz_cama@hotmail.com>
2018-10-17 15:04:59 +0200

    select all prereqs of skill

--- public/js/character/createPlayerCharSkills.js
+++ public/js/character/createPlayerCharSkills.js
@@ -241,9 +241,9 @@ var CreatePlayerCharSkills = new function(){
 				if(problemArray.indexOf(prereq_skill['name']) === -1
 					&& problem2Array.indexOf(prereq_skill['name']) === -1){
 						if($("character_class_skill_options [data-id="+ prereq_skill['id'] +"]")){
-							neededPrereqsTotalEP += $("character_class_skill_options [data-id="+ prereq_skill['id'] +"]").data('ep_cost');
+							neededPrereqsTotalEP += $("#character_class_skill_options [data-id="+ prereq_skill['id'] +"]").data('ep_cost');
 						} else {
-							neededPrereqsTotalEP += $("character_non_class_skill_options [data-id="+ prereq_skill['id'] +"]").data('ep_cost');
+							neededPrereqsTotalEP += $("#character_non_class_skill_options [data-id="+ prereq_skill['id'] +"]").data('ep_cost');
 						}
 						problemSkillIds.push(prereq_skill['id']);
 						problemArray.push(prereq_skill['name']);
@@ -267,9 +267,9 @@ var CreatePlayerCharSkills = new function(){
 					if(problemArray.indexOf(prereq_skill['name']) === -1
 						&& problem2Array.indexOf(prereq_skill['name']) === -1){
 							if($("character_class_skill_options [data-id="+ prereq_skill['id'] +"]")){
-								neededPrereqsTotalEP += $("character_class_skill_options [data-id="+ prereq_skill['id'] +"]").data('ep_cost');
+								neededPrereqsTotalEP += $("#character_class_skill_options [data-id="+ prereq_skill['id'] +"]").data('ep_cost');
 							} else {
-								neededPrereqsTotalEP += $("character_non_class_skill_options [data-id="+ prereq_skill['id'] +"]").data('ep_cost');
+								neededPrereqsTotalEP += $("#character_non_class_skill_options [data-id="+ prereq_skill['id'] +"]").data('ep_cost');
 							}
 								problemSkillIds.push(prereq_skill['id']);
 							problem2Array.push(prereq_skill['name']);
