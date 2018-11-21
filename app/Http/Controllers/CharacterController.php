6a8e4b4a82374d22b666edece3cd898d02e60715
jvanrosmalen <jazz_cama@hotmail.com>
2018-11-21 20:02:30 +0100

    show multiple chars for player

--- app/Http/Controllers/CharacterController.php
+++ app/Http/Controllers/CharacterController.php
@@ -260,8 +260,43 @@ class CharacterController extends Controller
 	}
 
 	public function showUserCharacter($userId, $charId){
-		echo $userId;
-		echo $charId;
+		$user = Auth::user();
+    	
+    	if($user->id != $userId){
+			$character = Character::where('user_id', '=', $user->id )
+			->where('id', '=', $charId)
+			->where('is_alive', true)
+			->where('is_player_char', true)
+			->first();
+		
+			if($character != null){
+				$skill_handout_objects = array();
+				$char_docs = array();
+
+				if(Storage::disk('chardocs')->exists('/'.$character->id)){
+					$full_names = glob(storage_path('app/chardocs/'.$character->id).'/*.pdf');
+
+					// Now strip off leading directory path
+					foreach($full_names as $full_name){
+						$char_docs[] = basename($full_name);
+					}
+				}
+				
+				$handoutSkills = $character->skills()->whereNotNull("skill_handout")->where('skill_handout', '!=', '')->get();
+
+				foreach($handoutSkills as $handoutSkill){
+					$skill_handout_objects[] = ["skill_id" => $handoutSkill->id, "handout_name" => $handoutSkill->skill_handout];
+				}
+
+				return view('character/showPlayerChar', ['character'=>$character,
+					'overview_skills_string_array' => $character->getOverviewSkillsStringArray(),
+					'skill_handouts' => $skill_handout_objects,
+					'char_docs' => $char_docs
+					]);
+			}
+		}else{
+			return redirect('/illegal_link');
+		}
 	}
     
     public function showEditPlayerChar($charId){
