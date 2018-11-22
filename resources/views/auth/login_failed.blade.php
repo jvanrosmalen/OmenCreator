71f2ffff52925a2601416456c542345e293ff294
jvanrosmalen <jazz_cama@hotmail.com>
2018-11-22 08:32:10 +0100

    typo

--- resources/views/auth/login_failed.blade.php
+++ resources/views/auth/login_failed.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-6 col-md-offset-3">
             <div class="panel panel-default">
-                <div class="panel-heading">Alh&eacute;nnia zegt:</div>
+                <div class="panel-heading">Ahl&eacute;nnia zegt:</div>
 
                 <div class="panel-body text-center">
                     Ik ken jouw naam niet, en de woorden die je spreekt kunnen mij niet bekoren.<br>
