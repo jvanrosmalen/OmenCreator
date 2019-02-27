22e047147d0cf9a552b95503bda229ed88bf6abd
jvanrosmalen <jazz_cama@hotmail.com>
2019-02-27 14:24:59 +0000

    add laravel-cors

--- config/app.php
+++ config/app.php
@@ -158,6 +158,7 @@ return [
         App\Providers\RouteServiceProvider::class,
 
 	Barryvdh\DomPDF\ServiceProvider::class,
+	Barryvdh\Cors\ServiceProvider::class,
     ],
 
     /*
---
+++ config/cors.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    /*
+     |--------------------------------------------------------------------------
+     | Laravel CORS
+     |--------------------------------------------------------------------------
+     |
+     | allowedOrigins, allowedHeaders and allowedMethods can be set to array('*')
+     | to accept any value.
+     |
+     */
+    'supportsCredentials' => false,
+    'allowedOrigins' => ['dev.omencreator.be'],
+    'allowedHeaders' => ['*'],
+    'allowedMethods' => ['*'],
+    'exposedHeaders' => [],
+    'maxAge' => 0,
+    'hosts' => [],
+];
+
