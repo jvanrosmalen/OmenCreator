c084e6a70bbf9d26f03f237190c8cdabaf24dae2
jvanrosmalen <jazz_cama@hotmail.com>
2019-03-01 15:13:58 +0100

    add events

--- app/Http/Controllers/LarpEventController.php
+++ app/Http/Controllers/LarpEventController.php
@@ -5,12 +5,13 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use App\LarpEvent;
 
 class LarpEventController extends Controller
 {
     public function showAllEvents(){
-        $coming_events = LarpEvents::all()->where('end_date','>',now())->get();
-        $past_events = LarpEvents::all()->where('end_date','<',now())->get();
+        $coming_events = LarpEvent::all()->where('end_date','>',now())->get();
+        $past_events = LarpEvent::all()->where('end_date','<',now())->get();
 
         return view('larp_event\showAllLarpEvents',
                         ['coming_events' => $coming_events,
