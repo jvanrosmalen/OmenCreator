529474d2e6ef3747c3d7a9f4fca0b392982af102
jvanrosmalen <jazz_cama@hotmail.com>
2019-03-22 15:15:23 +0100

    Create Event

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,6 +26,8 @@
 
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.5.0/js/bootstrap-datepicker.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/locale/nl.js"></script>
 
 	<!-- All other stuff -->
 	<link href="{{ URL::asset('css/create.css') }}" rel='stylesheet' type='text/css'>
