cc195e4edf3d0c5abcb32096f289a28f8f6cb283
jvanrosmalen <jazz_cama@hotmail.com>
2019-03-29 15:07:03 +0100

    event create submit

--- app/LarpEvent.php
+++ app/LarpEvent.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class LarpEvent extends Model
 {
+    public $timestamps = false; 
+
     public function characters(){
         return $this->belongsToMany('App\Character')->withTimeStamps();
     }
