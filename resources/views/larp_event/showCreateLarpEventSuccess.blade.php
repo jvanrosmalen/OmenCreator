124017f82579356fe88a59781ce0c726a4a96e5c
jvanrosmalen <jazz_cama@hotmail.com>
2019-04-05 13:26:52 +0200

    larp events

--- resources/views/larp_event/showCreateLarpEventSuccess.blade.php
+++ resources/views/larp_event/showCreateLarpEventSuccess.blade.php
@@ -11,27 +11,23 @@
 	<div class="row"></div>
 
 	<div class="row well">
-		<div class='row'>
+        <div class='row'>
+            <div class='row'>
+            </div>
 			<div class="col-xs-10 col-xs-offset-1">
-				Het event met de naam:<br>
+				Het event met de naam: <b>{{ $eventName }}</b> is succesvol gecre&euml;erd.
 			</div>
-			<div class="col-xs-10 col-xs-offset-2">
-				<div class='row'>
-                    {{ $eventName }}
-				</div>
-			</div>
-			<div class="col-xs-10 col-xs-offset-1">
-				is succesvol gecre&euml;erd.
-			</div>
+            <div class='row'>
+            </div>
 		</div>
 	</div>
 	
 	<div class="row">
 		<div class="row button-row">
 			<div class="col-xs-2 col-xs-offset-5">
-				<a href="{{ url('/larpeventsshowall') }}" class="btn btn-default"
+				<a href="{{ url('/larpeventsshowall') }}" class="btn btn-success"
 					type="button"
-					style="width: 100%; font-size: 18px;">Naar Event Overzicht</a>
+					style="width: 100%; font-size: 18px;">Event Overzicht</a>
 			</div>
 		</div>
 
