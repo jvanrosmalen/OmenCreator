b179d2106dd5f880795faca13d8d4c26eb7986c9
jvanrosmalen <jazz_cama@hotmail.com>
2019-10-11 15:34:56 +0200

    event delete

--- public/js/larpevent/larpEvent.js
+++ public/js/larpevent/larpEvent.js
@@ -20,6 +20,14 @@ var LarpEvent = new function(){
         $(this).removeClass("selected");
         var char_id = $(this).attr('id');
         $(("#participant_"+char_id)).removeClass("hidden");
+
+        // Add id to submit array
+        var selected_participants_list = JSON.parse($("#selected_participants").val());
+        var indexOfCharId = selected_participants_list.indexOf(char_id);
+        if(indexOfCharId < 0){
+          // char id is not yet in array. Add it.
+          selected_participants_list.push(char_id);
+        } 
       });
       
       ParticipantSelector.closeParticipantSelector(event);
@@ -32,5 +40,13 @@ var LarpEvent = new function(){
       var id = $(buttonSource).data("id");
       $(("#participant_"+id)).addClass("hidden");
       $("#"+id).removeClass("hidden");
+
+      // Remove id from the submit array
+      var selected_participants_list = JSON.parse($("#selected_participants").val());
+      var indexOfCharId = selected_participants_list.indexOf(id);
+      if(indexOfCharId < 0){
+        // char id is in the array. Remove it.
+        selected_participants_list.splice(indexOfCharId, 1);
+      }
     }
 }
